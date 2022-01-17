c3ab434b39ad08034815e28d92d7624ad123fde9
ThomasQuesadilla <quintanillathomas00@gmail.com>
2022-01-17 15:21:58 -0500

    update table structures, add indices for students and routes

--- ht_buses_app/models.py
+++ ht_buses_app/models.py
@@ -13,20 +13,24 @@ class Student(models.Model):
     school_id = models.ForeignKey(School, default=0, on_delete=models.CASCADE)
     student_school_id = models.IntegerField(default=0)
     route_id = models.ForeignKey(Route, default=0, on_delete=models.SET(0))
+    user_extended_id = models.ForeignKey(UserExtended, default=0, on_delete=models.CASCADE) 
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['route_id'])
+            models.Index(fields=['school_id'])
+            models.Index(fields=['user_extended_id'])
+        ]
 
 class Route(models.Model):
     name = models.CharField(max_length=25)
     school_id = models.ForeignKey(School, default=0, on_delete=models.CASCADE)
-    user_student_group_id = models.ForeignKey(user_student_group_id, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
-
-class RouteStudentGroup(models.Model):
-    student_id = models.ForeignKey(Student, default=None, on_delete=models.PROTECTED)
-
-class UserStudentGroup(models.Model):
-    student_id = models.ForeignKey(Student, default=None, on_delete=models.PROTECTED)
+    class Meta:
+        indexes = [
+            models.Index(fields=['school_id'])
+        ]
 
 class UserExtended(models.Model):
     # use is_staff for admin login
     address = models.CharField(max_length=100)
-    user_student_group_id = models.ForeignKey(UserStudentGroup, default=None, on_delete=models.CASCADE)
