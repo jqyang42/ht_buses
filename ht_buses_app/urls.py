6e61a244cfd3245feba7ae3e08b6d7953ad8202c
Kyra Chan <ktc22@duke.edu>
2022-01-17 14:22:02 -0500

    add views

--- ht_buses_app/urls.py
+++ ht_buses_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('students', views.students, name='students'),
+    path('signup', views.signup, name="signup"),
 ]
--- ht_buses_app/views.py
+++ ht_buses_app/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html', {})
+
+def students(request):
+    return render(request, 'students.html', {})
+
+def signup(request):
+    return render(request, 'signup.html', {})
