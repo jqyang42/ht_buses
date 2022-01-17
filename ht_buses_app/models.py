eb576961f742b86237bc96d484328499577d7a09
ThomasQuesadilla <quintanillathomas00@gmail.com>
2022-01-17 15:43:23 -0500

    syntax errors in models, adds first migration

--- ht_buses_app/models.py
+++ ht_buses_app/models.py
@@ -12,13 +12,13 @@ class Student(models.Model):
     last_name = models.CharField(max_length=100)
     school_id = models.ForeignKey(School, default=0, on_delete=models.CASCADE)
     student_school_id = models.IntegerField(default=0)
-    route_id = models.ForeignKey(Route, default=0, on_delete=models.SET(0))
-    user_extended_id = models.ForeignKey(UserExtended, default=0, on_delete=models.CASCADE) 
+    route_id = models.ForeignKey('Route', default=0, on_delete=models.SET(0))
+    user_extended_id = models.ForeignKey('UserExtended', default=0, on_delete=models.CASCADE) 
     
     class Meta:
         indexes = [
-            models.Index(fields=['route_id'])
-            models.Index(fields=['school_id'])
+            models.Index(fields=['route_id']),
+            models.Index(fields=['school_id']),
             models.Index(fields=['user_extended_id'])
         ]
 
