ff9ff9a3458b7e7c11e96382f727eca2660131d3
Kyra Chan <ktc22@duke.edu>
2022-01-19 21:04:03 -0500

    finish detailing templates

--- ht_buses_app/urls.py
+++ ht_buses_app/urls.py
@@ -4,9 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('students', views.students, name='students'),
+    path('students_detail', views.students_detail, name="students_detail"),
     path('signup', views.signup, name="signup"),
     path('schools', views.schools, name="schools"),
+    path('schools_detail', views.schools_detail, name="schools_detail"),
     path('routes', views.routes, name="routes"),
+    path('routes_detail', views.routes_detail, name="routes_detail"),
     path('users', views.users, name="users"),
+    path('users_detail', views.users_detail, name="users_detail"),
     path('routeplanner', views.routeplanner, name="routeplanner"),
 ]
--- ht_buses_app/views.py
+++ ht_buses_app/views.py
@@ -7,17 +7,29 @@ def index(request):
 def students(request):
     return render(request, 'students.html', {})
 
+def students_detail(request):
+    return render(request, 'students_detail.html', {})
+
 def signup(request):
     return render(request, 'signup.html', {})
 
 def schools(request):
     return render(request, 'schools.html', {})
 
+def schools_detail(request):
+    return render(request, 'schools_detail.html', {})
+
 def routes(request):
     return render(request, 'routes.html', {})
 
+def routes_detail(request):
+    return render(request, 'routes_detail.html', {})
+
 def users(request):
     return render(request, 'users.html', {})
+
+def users_detail(request):
+    return render(request, 'users_detail.html', {})
 
 def routeplanner(request):
     return render(request, 'route_planner.html', {})
