a403bd0162b6688adb626f5b01f386aa7fbc4c85
Kyra Chan <ktc22@duke.edu>
2022-01-22 18:37:51 -0500

    install bootstrap icons, move all table views to react

--- frontend/src/templates/index.js
+++ frontend/src/templates/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
 import HT_Logo from '../static/img/HT_Logo.png';
 
-const siteName = "Hypothetical Transportation Bus Management System";
-
-//class component
 class Login extends Component {
 	render() {
 		return (
