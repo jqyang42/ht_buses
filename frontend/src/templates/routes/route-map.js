3776199ab6ec8bb430acf1f1ef17ecd06ad6c147
ThomasQuesadilla <quintanillathomas00@gmail.com>
2022-01-27 17:22:46 -0500

    renders markers based on location of parents

--- frontend/src/templates/routes/route-map.js
+++ frontend/src/templates/routes/route-map.js
@@ -1,6 +1,8 @@
+import axios from "axios";
 import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { GOOGLE_API_KEY } from '../../constants';
+import { GOOGLE_API_KEY, API_DOMAIN } from '../../constants';
+import Geocode from "react-geocode";
 
 const containerStyle = {
   width: '100%',
@@ -16,14 +18,36 @@ class RouteMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      icon: "https://www.google.com/mapfiles/marker.png"
+      icon: "https://www.google.com/mapfiles/marker.png",
+      locations: [],
+      latLngs: [],
     }
+  }
+  componentDidMount() {
+    axios.get(API_DOMAIN + `schools/routes_planner?id=1`)
+      .then(res => {
+        const locations = res.data;
+        console.log(locations)
+        this.setState({ locations });
+      })
   }
   handleClick = (e) => {
     this.setState({
       icon: "https://www.google.com/mapfiles/marker_yellow.png"
     })
-
+  }
+  handleLoad = (e) => {
+    for (const [index, value] of this.state.locations) {
+      Geocode.fromAddress(value.address).then(
+        (response) => {
+          const { lat, lng } = response.results[0].geometry.location;
+          latLngs.push({ lat, lng });
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
   }
   render() {
     return (
@@ -35,15 +59,17 @@ class RouteMap extends Component {
             mapContainerStyle={containerStyle}
             center={center}
             zoom={15}
+            onLoad={handleLoad}
           >
             { /* Child components, such as markers, info windows, etc. */}
-            <Marker position={center} icon={this.state.icon} onClick={this.handleClick} />
+            {this.state.routes.map((value, index) => {
+              <Marker position={this.state.latLngs[index]} icon={this.state.icon} onClick={this.handleClick} id={value.students[0].id} />
+            })}
           </GoogleMap>
         </LoadScript>
       </div>
     )
   }
-
 }
 
 export default React.memo(RouteMap)
