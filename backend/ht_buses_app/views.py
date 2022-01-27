7c0014da94ab2b959041d11563725fc6a95a1103
Jessica Yang <jessica.yang@duke.edu>
2022-01-26 22:42:45 -0500

    student details connected

--- backend/ht_buses_app/views.py
+++ backend/ht_buses_app/views.py
@@ -82,7 +82,7 @@ def students_detail(request):
     data = {}
     id = request.query_params["id"]
     # Cannot be tested, need API for creating a parent
-    Student.studentsTable.create(first_name="Mary", last_name="Jane", school_id=School.schoolsTable.get(pk=1), student_school_id=3,route_id=Route.routeTables.get(pk=1),user_id=User.objects.get(pk=1))
+    # Student.studentsTable.create(first_name="Mary", last_name="Jane", school_id=School.schoolsTable.get(pk=1), student_school_id=3,route_id=Route.routeTables.get(pk=1),user_id=User.objects.get(pk=1))
     student = Student.studentsTable.get(pk=id)
     student_serializer = StudentSerializer(student, many=False)
     route = Route.routeTables.get(pk=student_serializer.data["route_id"])
