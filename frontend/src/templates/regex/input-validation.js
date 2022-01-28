d1e6974575d0bd9d3340b717f85dc6ec90e69368
ferncorona <fec6@duke.edu>
2022-01-28 01:52:20 -0500

    password + email field validation + confirmation

---
+++ frontend/src/templates/regex/input-validation.js
@@ -0,0 +1,9 @@
+export const emailRegex = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+ );
+ export const passwordRegex = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$');
+
+ //(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\])
+ 
+ 
+ 
