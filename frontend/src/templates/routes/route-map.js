b4b5eac8071d28fe38f09ff14a0de0b3bbfea077
ThomasQuesadilla <quintanillathomas00@gmail.com>
2022-01-27 23:21:02 -0500

    messing with custom coloring

--- frontend/src/templates/routes/route-map.js
+++ frontend/src/templates/routes/route-map.js
@@ -2,26 +2,45 @@ import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { GOOGLE_API_KEY } from '../../constants';
 
+const x = 50;
+const y = 50;
 const containerStyle = {
   width: '100%',
   height: '400px'
 };
-
+const greatPlaceStyle = {
+  width: '200px'
+}
 const center = {
   lat: -3.745,
   lng: -38.523
 };
+const pinSVGHole = "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z";
 
 class RouteMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      icon: "https://www.google.com/mapfiles/marker.png"
+      icon: {
+        path: pinSVGHole,
+        fillOpacity: 1,
+        fillColor: "green",
+        strokeWeight: 2,
+        strokeColor: "#000000",
+        scale: 2,
+      }
     }
   }
   handleClick = (e) => {
     this.setState({
-      icon: "https://www.google.com/mapfiles/marker_yellow.png"
+      icon: {
+        path: pinSVGHole,
+        fillOpacity: 1,
+        fillColor: "red",
+        strokeWeight: 2,
+        strokeColor: "#000000",
+        scale: 2,
+      }
     })
 
   }
@@ -37,7 +56,7 @@ class RouteMap extends Component {
             zoom={15}
           >
             { /* Child components, such as markers, info windows, etc. */}
-            <Marker position={center} icon={this.state.icon} onClick={this.handleClick} />
+            <Marker position={center} id='icon' icon={this.state.icon} onClick={this.handleClick} style={greatPlaceStyle} />
           </GoogleMap>
         </LoadScript>
       </div>
