e0a6c462563819484274d6b78d94792f30ca33ed
Jessica Yang <jessica.yang@duke.edu>
2022-01-28 16:00:02 -0500

    names merged

--- frontend/src/templates/tables/students-table.js
+++ frontend/src/templates/tables/students-table.js
@@ -12,7 +12,7 @@ export function StudentsTable( {data} ) {
             },
             {
                 Header: 'Name',
-                accessor: 'name',
+                accessor: d => `${d.first_name} ${d.last_name}`,
             },
             {
                 Header: 'School',
@@ -25,7 +25,7 @@ export function StudentsTable( {data} ) {
             },            
             {
                 Header: 'Parent Name',
-                accessor: 'parent_name',
+                accessor: d => `${d.parent.first_name} ${d.parent.last_name}`,
                 disableSortBy: true
             },
         ],
