c284702b288bf6d1bc25bd4349e9cb65dbe41881
Jessica Yang <jessica.yang@duke.edu>
2022-02-01 20:51:16 -0500

    updated urls

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -15,7 +15,7 @@ export const MARKER_COLORS = colors;
 export const MARKER_ICONS = marker_icons;
 
 export const SITE_NAME = "Hypothetical Transportation Bus Management System";
-export const API_DOMAIN = "http://localhost:8000/"
+export const API_DOMAIN = "http://localhost:8000/api/"
 
 // Link URLs
 export const INDEX_URL = "/";
