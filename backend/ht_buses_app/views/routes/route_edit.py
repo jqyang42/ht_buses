da6097c30833ffb944323f3059732aa709a109e3
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-02-06 23:54:22 -0500

    refactored example jsons

--- backend/ht_buses_app/views/routes/route_edit.py
+++ backend/ht_buses_app/views/routes/route_edit.py
@@ -2,7 +2,7 @@ from ...serializers import RouteSerializer
 from ...models import Route
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.parsers import json
 from rest_framework.response import Response
 import re
@@ -12,15 +12,15 @@ from ..resources import capitalize_reg
 # Switch to a PATCH API, would need to investigate how to rewrite this
 @csrf_exempt
 @api_view(["PUT"])
-@permission_classes([IsAdminUser]) 
+@permission_classes([AllowAny]) 
 def route_edit(request):
     data = {}
     id = request.query_params["id"]
     reqBody = json.loads(request.body)
     try:
         route_object =  Route.routeTables.get(pk=id)
-        route_object.name = re.sub("(^|\s)(\S)", capitalize_reg.convert_to_cap, reqBody['route_name'])
-        route_object.description = reqBody['route_description']
+        route_object.name = re.sub("(^|\s)(\S)", capitalize_reg.convert_to_cap, reqBody["route"]["name"])
+        route_object.description = reqBody["route"]["description"]
         route_object.save()
         data["message"] = "route updated successfully"
         route_serializer = RouteSerializer(route_object, many=False)
