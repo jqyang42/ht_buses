8f831d2f719a35a7449599c9b05c059691e63b47
Jessica Yang <jessicayang42@gmail.com>
2022-02-07 16:04:55 -0500

    working with nginx and react?? am i?? a genius???

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -15,7 +15,7 @@ export const MARKER_COLORS = colors;
 export const MARKER_ICONS = marker_icons;
 
 export const SITE_NAME = "Hypothetical Transportation Bus Management System";
-export const API_DOMAIN = "http://localhost:8000/api/"
+export const API_DOMAIN = "http://localhost:1337/api/"
 
 // Link URLs
 export const INDEX_URL = "/";
