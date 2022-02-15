3064b7503f0b9193c5a72dce322ccf5884e00613
Kyra Chan <ktc22@duke.edu>
2022-02-15 12:18:35 -0500

    add circle next to route labels on students tables

--- frontend/src/templates/tables/table.js
+++ frontend/src/templates/tables/table.js
@@ -101,7 +101,7 @@ export function Table({ columns, data, searchOn, searchLabel, ourGlobalFilterFun
                         {row.cells.map(cell => {
                         return (navUrl==="/students/" || "/routes/") ? 
                         <td {...cell.getCellProps()} className={(row.original.route_name === "Unassigned" || row.original.route_name === "Out of Range" || row.original.route_name === "Incomplete") ? `${cell.column.className ?? ""}` : ""}> {cell.render('Cell')}</td> : 
-                        <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
+                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                         })}
                     </tr>
                     )
@@ -114,7 +114,7 @@ export function Table({ columns, data, searchOn, searchLabel, ourGlobalFilterFun
                         {row.cells.map(cell => {
                         return (navUrl==="/students/") ?
                         <td {...cell.getCellProps()} className={(row.original.route_name === "Unassigned" || row.original.route_name === "Out of Range" || row.original.route_name === "Incomplete") ? `${cell.column.className ?? ""}` : ""}> {cell.render('Cell')}</td> :
-                        <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
+                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                         })}
                     </tr>
                     )
