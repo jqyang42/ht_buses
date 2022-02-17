97d65a4f81da8d564a52ee8e688fec1c2da65c8e
ferncorona <fec6@duke.edu>
2022-02-17 14:21:05 -0500

    fixed merge conflicts

--- frontend/src/templates/general/reset-password.js
+++ frontend/src/templates/general/reset-password.js
@@ -28,6 +28,7 @@ class ResetPassword extends Component {
         this.change_password().then(password_changed => {
            //Show that password was successfully reset 
         })
+    }
 
     sendApiRequest = () => {
 
