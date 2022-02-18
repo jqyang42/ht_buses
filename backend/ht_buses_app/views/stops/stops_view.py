7053c818606cf74a3285252c04b7fc92778f1917
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-02-18 00:18:25 -0500

    Fixed stop view permissions

--- backend/ht_buses_app/views/stops/stops_view.py
+++ backend/ht_buses_app/views/stops/stops_view.py
@@ -3,14 +3,12 @@ from ...serializers import StopSerializer, LocationSerializer
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import IsAdminUser, AllowAny
-from rest_framework.parsers import json
 from rest_framework.response import Response
-from datetime import datetime
 
 # Stops GET API
 @csrf_exempt
 @api_view(["GET"])
-@permission_classes([AllowAny]) 
+@permission_classes([IsAdminUser]) 
 def stops_view(request):
     data = {}
     id = request.query_params["id"] # this is route id
