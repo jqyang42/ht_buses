40635eeb76b8e47b0c6d90540ad29165ffb8d2a4
Jessica Yang <jessica.yang@duke.edu>
2022-02-18 16:58:00 -0500

    render

--- frontend/src/templates/routes/route-time-calc.js
+++ frontend/src/templates/routes/route-time-calc.js
@@ -1,26 +1,48 @@
 import { DistanceMatrixService } from "@react-google-maps/api";
+import React, { Component } from "react";
 
-function calcTime({ origin, destination }) {
-    // return (
-    //     <DistanceMatrixService 
-    //     options={{
-    //         destinations: destination, // destination should be array of lat/lng dicts
-    //         origins: origin, // same as destination
-    //         travelMode: "DRIVING",
-    //     }}
-    //     callback={(response) => updateTimes({ response: response })}
-    //     />
-    // )
-    DistanceMatrixService(origin, destination, "DRIVING", (res, status) => {
-        console.log(durations)
-        console.log(status)
-    })
+class TimeCalculation extends Component {
 
-    // https://medium.com/@chan.buena/how-to-use-google-distance-matrix-api-on-front-end-or-back-end-with-react-72d342f05733
+    updateTimes = (response) => {
+        console.log(response)
+        const index = 0 // index of destination
+        const travel_time = response.rows[0].elements[index].duration.text  // time to travel from origin to desitnation
+    }
+
+    render() {
+        return (
+            <DistanceMatrixService 
+            options={{
+                destinations: destination, // destination should be array of lat/lng dicts
+                origins: origin, // same as destination
+            }}
+            callback={(response) => updateTimes(response)}
+            />
+        )
+    }
 }
 
-function updateTimes({ response }) {
-    console.log(response)
-    const index = 0 // index of destination
-    const travel_time = response.rows[0].elements[index].duration.text  // time to travel from origin to desitnation
-}
+// function calcTime({ origin, destination }) {
+//     // return (
+//     //     <DistanceMatrixService 
+//     //     options={{
+//     //         destinations: destination, // destination should be array of lat/lng dicts
+//     //         origins: origin, // same as destination
+//     //         travelMode: "DRIVING",
+//     //     }}
+//     //     callback={(response) => updateTimes({ response: response })}
+//     //     />
+//     // )
+//     DistanceMatrixService(origin, destination, "DRIVING", (res, status) => {
+//         console.log(durations)
+//         console.log(status)
+//     })
+
+//     // https://medium.com/@chan.buena/how-to-use-google-distance-matrix-api-on-front-end-or-back-end-with-react-72d342f05733
+// }
+
+// function updateTimes({ response }) {
+//     console.log(response)
+//     const index = 0 // index of destination
+//     const travel_time = response.rows[0].elements[index].duration.text  // time to travel from origin to desitnation
+// }
