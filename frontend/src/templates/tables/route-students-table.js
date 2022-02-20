854343d35bfd1ea52cba11a880ecdeb649a108fc
Kyra Chan <ktc22@duke.edu>
2022-02-20 01:04:58 -0500

    fix bug

--- frontend/src/templates/tables/route-students-table.js
+++ frontend/src/templates/tables/route-students-table.js
@@ -22,7 +22,7 @@ export function RouteStudentsTable({ data, showAll }) {
             },
             {
                 Header: 'Bus Stops',
-                accessor: 'in_range',
+                accessor: 'route.in_range',
                 disableFilter: true,
                 Cell: ({ cell: { value } }) => (
                     value ? <>{"In Range"}</> : <><div className="unassigned">{"Out of Range"}</div></>
