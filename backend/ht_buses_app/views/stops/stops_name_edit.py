f6971fcf4a6651e4994492b57208c29051b4ac50
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-02-20 16:45:41 -0500

    Stop API's complete

--- backend/ht_buses_app/views/stops/stops_name_edit.py
+++ backend/ht_buses_app/views/stops/stops_name_edit.py
@@ -1,17 +1,16 @@
-from ...models import Stop, Route
+from ...models import Stop
 from ...serializers import StopSerializer
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.parsers import json
 from rest_framework.response import Response
-from datetime import datetime
 
 # Stops PUT API
 @csrf_exempt
 @api_view(["PUT"])
 @permission_classes([IsAdminUser]) 
-def stops_edit(request):
+def stops_name_edit(request):
     data = {}
     try:
         reqBody = json.loads(request.body)
@@ -19,17 +18,10 @@ def stops_edit(request):
         stops = []
         for stop in reqBody["stops"]:
             stop_obj = Stop.stopTables.get(pk=stop["id"])
-            print(stop_obj)
-            route = Route.routeTables.get(pk=stop["route_id"])
-            stop_obj.route_id = route
-            stop_obj.order_by = count
-            arrival = stop["arrival"]
-            stop_obj.arrival = datetime.time(datetime.strptime(arrival,"%H:%M"))
-            departure = stop["departure"]
-            stop_obj.departure = datetime.time(datetime.strptime(departure, "%H:%M"))
-            stop_obj.location_id.address = ""
-            stop_obj.location_id.lat = stop["lat"]
-            stop_obj.location_id.long = stop["long"]
+            if stop["name"] == "" or stop["name"] == None:
+                stop_obj.name = "Stop " + str(stop_obj.order_by)
+            else:
+                stop_obj.name = stop["name"]
             stop_obj.save()
             stop_serializer = StopSerializer(stop_obj, many=False)
             stops.append(stop_serializer.data)
@@ -42,5 +34,3 @@ def stops_edit(request):
         data["message"] = "stops could not be edited"
         data["success"] = False
         return Response(data, status=400)
-
-
