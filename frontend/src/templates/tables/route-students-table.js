a549c4bbd3e687bffe694ae2d43d372e68a40ba0
Kyra Chan <ktc22@duke.edu>
2022-02-20 01:22:15 -0500

    test out value print

--- frontend/src/templates/tables/route-students-table.js
+++ frontend/src/templates/tables/route-students-table.js
@@ -25,7 +25,7 @@ export function RouteStudentsTable({ data, showAll }) {
                 accessor: 'in_range',
                 disableFilter: true,
                 Cell: ({ cell: { value } }) => (
-                    value ? <>{"In Range"}</> : <><div className="unassigned">{"Out of Range"}</div></>
+                    value ? <>{"In Range"}</> : <><div className="unassigned">{value}</div></>
                 )
             },
         ],
