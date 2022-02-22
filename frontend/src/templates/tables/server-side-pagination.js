5ddd193bd1c8e72eb06b0c3a0884bda5b775a078
Jessica Yang <jessica.yang@duke.edu>
2022-02-22 15:22:57 -0500

    search up for students

--- frontend/src/templates/tables/server-side-pagination.js
+++ frontend/src/templates/tables/server-side-pagination.js
@@ -8,17 +8,13 @@ export async function getPage({ url, pageIndex, sortOptions, searchValue }) {
 
     let response
     if (!order_by || order_by === 'none') {
-        // if (!search) {
-        //     response = await api.get(`${url}?page=${pageIndex}`)
-        // } else {
-            response = await api.get(`${url}?page=${pageIndex}&q=${search}`)
-        // }
+        if (search === '') {
+            response = await api.get(`${url}?page=${pageIndex}`)
+        } else {
+            response = await api.get(`${url}/search?page=${pageIndex}&q=${search}`)
+        }
     } else {
-        // if (!search) {
-        //     response = await api.get(`${url}/sort?page=${pageIndex}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}`)
-        // } else {
-            response = await api.get(`${url}/sort?page=${pageIndex}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}&q=${search}`)
-        // }
+        response = await api.get(`${url}/sort?page=${pageIndex}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}&q=${search}`)
     }
 
     console.log(response)
