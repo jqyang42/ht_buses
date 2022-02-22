24f8a1086ecae58f7493f44ff62fbeae80c51f15
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-02-21 23:34:57 -0500

    Sorting complete for students

---
+++ backend/ht_buses_app/examples/students/requests/student_sorting.py
@@ -0,0 +1,6 @@
+# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=parent-name
+# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=route
+# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=bus-stop
+# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=name
+# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=ID
+# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=school
