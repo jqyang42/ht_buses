f39c256307857f75cf7c8d555622d5b39e5a7fe4
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-02-21 23:38:38 -0500

    example urls

--- backend/ht_buses_app/examples/students/requests/student_sorting.py
+++ backend/ht_buses_app/examples/students/requests/student_sorting.py
@@ -1,6 +1,7 @@
+# Example URLS :)
 # http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=parent-name
 # http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=route
 # http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=bus-stop
 # http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=name
 # http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=ID
-# http://localhost:8000/api/students/sort?page=1&order_by=asc&sort_by=school
+# http://localhost:8000/api/students/sort?page=1&order_by=desc&sort_by=school
