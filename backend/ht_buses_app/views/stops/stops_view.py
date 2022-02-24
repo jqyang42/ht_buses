a1e061376f8f14cb229b267e1bf340a6e6b8d7bf
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-02-23 23:47:04 -0500

    Added is complete field for route

--- backend/ht_buses_app/views/stops/stops_view.py
+++ backend/ht_buses_app/views/stops/stops_view.py
@@ -1,5 +1,5 @@
 from ...models import Stop, Location, Route
-from ...serializers import StopSerializer, LocationSerializer
+from ...serializers import StopSerializer, LocationSerializer, RouteSerializer
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import IsAdminUser, AllowAny
@@ -14,6 +14,7 @@ def stops_view(request):
     id = request.query_params["id"] # this is route id
     try:
         route = Route.routeTables.get(pk=id)
+        route_serializer = RouteSerializer(route, many=False)
         stops = Stop.stopTables.filter(route_id=route)
         # COMMENTED OUT CODE FOR PAGINATION
         # page_number = request.query_params["page"]
@@ -33,6 +34,7 @@ def stops_view(request):
             location_serializer = LocationSerializer(location, many=False)
             stops_arr.append({"id": id, "name": name, "arrival": arrival[:-3], "departure": departure[:-3], "location": location_serializer.data, "order_by": order_by})
         data["stops"] = stops_arr
+        data["route"] = {"id": id, "is_complete": route_serializer.data["is_complete"]}
         data["success"] = True
         return Response(data)
     except:
