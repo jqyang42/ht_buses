c76ba9fd48505270a1464c7c8be59362c21cb311
Jessica Yang <jessica.yang@duke.edu>
2022-02-24 03:07:17 -0500

    alphabetize

--- frontend/src/templates/components/dropdown.js
+++ frontend/src/templates/components/dropdown.js
@@ -2,7 +2,7 @@ import api from "./api"
 
 export async function makeParentsDropdown() {
     const res = await api.get(`users?page=0`)
-    return res.data.users.filter(user => {
+    const dropdown = res.data.users.filter(user => {
         return user.is_parent === true
     }).map(parent => {
         return { 
@@ -10,25 +10,31 @@ export async function makeParentsDropdown() {
             name: `${parent.first_name} ${parent.last_name}` 
         }
     })
+
+    return dropdown.sort()
 }
 
 export async function makeSchoolsDropdown() {
     const res = await api.get(`schools?page=0`)
-    return res.data.schools.map(school => {
+    const dropdown = res.data.schools.map(school => {
         return {
             value: school.id,
             display: school.name
         }
     })
+
+    return dropdown.sort()
 }
 
 export async function makeRoutesDropdown({ school_id }) {
     const res = await api.get(`schools/detail?id=${school_id}`)
     const routes_data = res.data?.routes || []
-    return routes_data.map(route => {
+    const dropdown = routes_data.map(route => {
         return {
             value: route.id,
             display: route.name
         }
     })
+
+    return dropdown.sort()
 }
