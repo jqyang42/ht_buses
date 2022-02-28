aee79824e30cbe336ad2feaa3bd4bb46e2247504
Jessica Yang <jessica.yang@duke.edu>
2022-02-28 14:11:43 -0500

    pagination up - users working correctly

--- frontend/src/templates/tables/server-side-pagination.js
+++ frontend/src/templates/tables/server-side-pagination.js
@@ -1,28 +1,49 @@
 import api from "../components/api";
 
+// additionalParams --> holds ?id=x
 export async function getPage({ url, pageIndex, sortOptions, searchValue, additionalParams }) {
-    const order_by = sortOptions?.sortDirection
-    const sort_by = sortOptions?.accessor
-    // const search = searchValue !== '' ? searchValue : false
-    const search = searchValue || ''
-    const params = additionalParams ? additionalParams : ''
+    const show_all = pageIndex === 0
+    const has_sort = sortOptions.sortDirection !== 'none'
+    const has_search = searchValue !== ''
+    
+    const order_by = has_sort ? sortOptions.sortDirection.toLowerCase() : ''
+    const sort_by = has_sort ? sortOptions.accessor : ''
 
-    let response
-    if (!order_by || order_by === 'none') {
-        if (search === '') {
-            response = await api.get(`${url}?page=${pageIndex}${params}`)
-        } else {
-            response = await api.get(`${url}/search?page=${pageIndex}&q=${search}`)
-        }
-    } else {
-        if (url === 'routes') {
-            response = await api.get(`${url}/sort?page=${pageIndex}&search=${searchValue !== ''}&q=${searchValue}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}`)
-        } else {
-            response = await api.get(`${url}/sort?page=${pageIndex}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}&q=${search}`)
-        }
-    }
+    const response = await api.get(`${url}?page=${pageIndex}&sort_by=${sort_by}&order_by=${order_by}&q=${searchValue}`)
+    // let response
+    // if (has_sort && has_search) {
+    //     response = await api.get(`${url}?page=${pageIndex}&sort_by=${sort_by}&order_by=${order_by}&q=${searchValue}`)
+    // } else if ()
 
-    http://localhost:8000/api/routes/sort?order_by=asc&sort_by=name&page=1&search=false&q=
+    // Only sorting
+    // url/api/users?page=1&sort_by=name&order_by=desc&q=
+    // No sorting or search
+    // url/api/users?page=1&sort_by=&order_by=&q=
+    // Sorting and search
+    // url/api/users?page=1&sort_by=name&order_by=asc&q=evelyn
+    // Only search
+    // url/api/users?page=1&sort_by=&order_by=&q=evelyn
+
+    // const order_by = sortOptions?.sortDirection
+    // const sort_by = sortOptions?.accessor
+    // // const search = searchValue !== '' ? searchValue : false
+    // const search = searchValue || ''
+    // const params = additionalParams ? additionalParams : ''
+
+    // let response
+    // if (!order_by || order_by === 'none') {
+    //     if (search === '') {
+    //         response = await api.get(`${url}?page=${pageIndex}${params}`)
+    //     } else {
+    //         response = await api.get(`${url}/search?page=${pageIndex}&q=${search}`)
+    //     }
+    // } else {
+    //     if (url === 'routes') {
+    //         response = await api.get(`${url}/sort?page=${pageIndex}&search=${searchValue !== ''}&q=${searchValue}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}`)
+    //     } else {
+    //         response = await api.get(`${url}/sort?page=${pageIndex}&order_by=${order_by.toLowerCase()}&sort_by=${sort_by}&q=${search}`)
+    //     }
+    // }
 
     console.log(response)
     return {
