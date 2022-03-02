fc8189f90f2191adc8f8499d48b9a7f4c6c1d4d7
ferncorona <fec6@duke.edu>
2022-03-02 03:42:30 -0500

    changed role param

--- backend/ht_buses_app/role_permissions.py
+++ backend/ht_buses_app/role_permissions.py
@@ -3,16 +3,16 @@ from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission): 
     def has_permission(self, request, view):
-        if request.user.role_id == 1:
+        if request.user.role == 1:
             return True
 
 
 class IsSchoolStaff(permissions.BasePermission): 
     def has_permission(self, request, view): 
-        if request.user.role_id == 3:
+        if request.user.role == 3:
             return True
 
 class IsDriver(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.role_id == 2:
+        if request.user.role == 2:
             return True
