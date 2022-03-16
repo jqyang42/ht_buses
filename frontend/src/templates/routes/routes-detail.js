0141d1c5c373ff45541fd13305e0c740f20d7500
Kyra Chan <ktc22@duke.edu>
2022-03-16 00:00:47 -0400

    update export pdf parent info

--- frontend/src/templates/routes/routes-detail.js
+++ frontend/src/templates/routes/routes-detail.js
@@ -18,6 +18,7 @@ class BusRoutesDetail extends Component {
     state = {
         route : [],
         students : [],
+        users: [],
         school : [],
         stops: null,
         center: {},
@@ -86,6 +87,7 @@ class BusRoutesDetail extends Component {
             
             this.setState({ 
                 students: students,
+                users: users,
                 route: route, 
                 school: school, 
                 center: { 
@@ -254,7 +256,7 @@ class BusRoutesDetail extends Component {
                                                     </span>
                                                 </Link> : ""
                                             }
-                                            <button type="button" className="btn btn-primary float-end w-auto me-3"  onClick={() => this.state.route.length !== 0 ? pdfRender(this.state.route, this.state.students) : ""}>
+                                            <button type="button" className="btn btn-primary float-end w-auto me-3"  onClick={() => this.state.route.length !== 0 ? pdfRender(this.state.route, this.state.users) : ""}>
                                                 <i className="bi bi-download me-2"></i>
                                                 Export
                                             </button>
