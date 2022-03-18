ceefdbaa767272c47b6c9f207c5adb4f782dbbd6
ThomasQuesadilla <quintanillathomas00@gmail.com>
2022-03-18 17:59:42 -0400

    merging

--- frontend/src/templates/tables/import-users-table.js
+++ frontend/src/templates/tables/import-users-table.js
@@ -2,6 +2,8 @@ import React, { useCallback, useEffect } from "react";
 import { Table } from "./table";
 import { useState } from "react";
 import { TableEditable } from "./table-editable";
+import Autocomplete from "react-google-autocomplete";
+import { GOOGLE_API_KEY } from "../../constants";
     
 export function ImportUsersTable({ data, showAll, pageIndex, canPreviousPage, canNextPage, 
     updatePageCount, pageSize, totalPages, searchValue, updateImportData }) {
@@ -63,15 +65,12 @@ export function ImportUsersTable({ data, showAll, pageIndex, canPreviousPage, ca
                     <div>
                         <Autocomplete
                         apiKey={GOOGLE_API_KEY}
-                        onPlaceSelected={this.handleAddressChange}
                         options={{
                             types: ['address']
                         }}
                         value={this.state.new_user?.location?.address}
                         defaultValue=""
                         placeholder="Enter home address" className="form-control pb-2" id="exampleInputAddress1"
-                        onChange={this.handleAddressChange}
-                        onBlur={event => {setTimeout(this.handleAddressValidation, 500)}}
                         required={this.state.new_user.is_parent} />
                     </div>
                 )
