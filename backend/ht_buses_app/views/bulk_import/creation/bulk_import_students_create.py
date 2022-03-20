69fe97a7b9d5c7681cb9f5e10442031c847a3dcd
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-03-20 11:44:08 -0400

    Added exclusion check to create and validate

--- backend/ht_buses_app/views/bulk_import/creation/bulk_import_students_create.py
+++ backend/ht_buses_app/views/bulk_import/creation/bulk_import_students_create.py
@@ -16,24 +16,25 @@ def students_create(request):
     reqBody = json.loads(request.body)
     for student in reqBody["students"]:
         # name, parent_email, student_id, school_name
-        name = student["name"].split(" ", 1)
-        first_name = name[0]
-        last_name = name[1]
-        student_school_id = student["student_id"]
-        school = School.objects.filter(name=student["school_name"])
-        school_serializer = SchoolSerializer(school, many=False)
-        student_school = School.objects.get(pk=school_serializer.data["id"])
-        user = User.objects.filter(email=student["parent_email"])[0]
-        user_serializer = UserSerializer(user, many=False)
-        parent = User.objects.get(pk=user_serializer.data["id"])
-        student = Student.objects.create(
-            first_name = first_name,
-            last_name = last_name,
-            student_school_id = student_school_id,
-            school_id = student_school,
-            route_id = None,
-            user_id = parent
-        )
+        if student["exclude"] == False:
+            name = student["name"].split(" ", 1)
+            first_name = name[0]
+            last_name = name[1]
+            student_school_id = student["student_id"]
+            school = School.objects.filter(name=student["school_name"])
+            school_serializer = SchoolSerializer(school, many=False)
+            student_school = School.objects.get(pk=school_serializer.data["id"])
+            user = User.objects.filter(email=student["parent_email"])[0]
+            user_serializer = UserSerializer(user, many=False)
+            parent = User.objects.get(pk=user_serializer.data["id"])
+            student = Student.objects.create(
+                first_name = first_name,
+                last_name = last_name,
+                student_school_id = student_school_id,
+                school_id = student_school,
+                route_id = None,
+                user_id = parent
+            )
     data["success"] = True
     data["student_count"] = len(reqBody["students"])
     return Response(data)
