bcf4a598cf507f320f238b875a13d075a7fcec28
ferncorona <fec6@duke.edu>
2022-03-20 15:25:16 -0400

    api changes for deleting a user

--- frontend/src/templates/components/api.js
+++ frontend/src/templates/components/api.js
@@ -14,22 +14,25 @@ const interceptor_api = axios.create({
       }
 });
 
-api.interceptors.request.use(async (config) => {
-    const res = await interceptor_api.get(`users/update-stored-info`)
-    const data = res.data.data
-    if(data.success) {
-        localStorage.setItem('user_id', data.user_id)
-        localStorage.setItem('role',  data.role_value)
-        localStorage.setItem('is_parent', data.is_parent)
-        localStorage.setItem('first_name', data.first_name)
-        localStorage.setItem('last_name', data.last_name)
-        localStorage.setItem('is_staff', data.role_id !== 4)
-        localStorage.setItem('logged_in', data.logged_in)
-        localStorage.setItem('token', res.data.token)
-    }
-    else {
-        localStorage.clear()
-    }
+api.interceptors.request.use( config => {
+    interceptor_api.get(`users/update-stored-info`)
+    .then(res => {
+        const data = res.data.data
+        if(data.success) {
+            localStorage.setItem('user_id', data.user_id)
+            localStorage.setItem('role',  data.role_value)
+            localStorage.setItem('is_parent', data.is_parent)
+            localStorage.setItem('first_name', data.first_name)
+            localStorage.setItem('last_name', data.last_name)
+            localStorage.setItem('is_staff', data.role_id !== 4)
+            localStorage.setItem('logged_in', data.logged_in)
+            localStorage.setItem('token', res.data.token)
+        }
+    }).catch (error => {
+        if (error.response.status !== 200) {
+            localStorage.clear()
+        }
+    })
     return config
 })
 
