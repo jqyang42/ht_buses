e7cf1e26bc4701a829a7ca7632feecb1b01300c1
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-03-19 23:53:53 -0400

    Fixed spacing

--- backend/ht_buses_app/views/bulk_import/upload/bulk_import_view_users.py
+++ backend/ht_buses_app/views/bulk_import/upload/bulk_import_view_users.py
@@ -76,7 +76,7 @@ def bulk_import(request):
                             address_str = address
                             no_address = ""
                         phone_number = user_serializer.data[0]["phone_number"]
-                        email_error_message = "Email already exists in the system as " + first_name + " " + last_name + " with " + no_address  + " address " + address_str + "and phone number " + phone_number
+                        email_error_message = "Email already exists in the system as " + first_name + " " + last_name + " with " + no_address  + " address " + address_str + " and phone number " + phone_number
                         email_error = True
         if row["name"] is None or row["name"] == "":
             name_error = True
