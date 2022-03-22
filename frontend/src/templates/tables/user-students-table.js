57dffc29ab4a9f8bd1eff1cc3f0bf1aedad63ebb
Kyra Chan <ktc22@duke.edu>
2022-03-22 01:41:27 -0400

    add schools column back into user-students

--- frontend/src/templates/tables/user-students-table.js
+++ frontend/src/templates/tables/user-students-table.js
@@ -20,10 +20,12 @@ export function UserStudentsTable({ data, showAll, pageIndex, canPreviousPage, c
                 id: 'name',
                 sortDirection: sort.accessor === 'student_school_id' ? sort.sortDirection : 'none'
             },
-            // {
-            //     Header: 'School',
-            //     accessor: 'school'
-            // },
+            {
+                Header: 'School',
+                accessor: 'school_name',
+                id: 'school',
+                sortDirection: sort.accessor === 'school' ? sort.sortDirection : 'none'
+            },
             {
                 Header: 'Bus Route',
                 accessor: d => Array(`${d.route.color_id}`,`${d.route.id}`, `${d.route.id != 0 ? d.route.name : ''}`),
@@ -50,24 +52,13 @@ export function UserStudentsTable({ data, showAll, pageIndex, canPreviousPage, c
     const columnHeaderClick = async (column) => {
         switch (column.sortDirection) {
           case 'none':
-            // console.log(column.sortDirection)
-            // console.log(column.id)
             setSort({ sortDirection: 'ASC', accessor: column.id });
-            // const desc = await getClients( 'ASC', column.id );
-            // setData(desc);
-            console.log(sort)
             break;
           case 'ASC':
             setSort({ sortDirection: 'DESC', accessor: column.id });
-            // const asc = await getClients('DESC', column.id);
-            console.log(sort)
-            // setData(asc);
             break;
           case 'DESC':
             setSort({ sortDirection: 'none', accessor: column.id });
-            // const newData = await getClients('none', column.id);
-            // setData(newData);
-            console.log(sort)
             break;
         }
     };
