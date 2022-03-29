3535bc2dc7179a1efef973c1a2769d105234bce3
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-03-28 22:55:21 -0400

    Fixed timezone problem with log creation

--- backend/ht_buses_app/views/logs/log_create.py
+++ backend/ht_buses_app/views/logs/log_create.py
@@ -2,11 +2,11 @@ from ...serializers import LogSerializer
 from ...models import User, Route, Log
 from rest_framework.response import Response
 import json
-from datetime import datetime
-from datetime import date
+from datetime import datetime, date, timezone
 from ...role_permissions import IsAdmin, IsDriver
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
+from pytz import timezone
 
 # Added IsAdmin so I can test on Postman so I don't have to switch to being a driver
 @csrf_exempt
@@ -15,10 +15,11 @@ from django.views.decorators.csrf import csrf_exempt
 def create_log(request):
     data = {}
     reqBody = json.loads(request.body)
+    edt = timezone('US/Eastern')
     log_obj = Log.objects.create(
         bus_number = reqBody["bus_number"],
-        date = date.today(),
-        start_time = datetime.now().time(),
+        date = datetime.now(edt).date(),
+        start_time = datetime.now(edt).time(),
         user_id = User.objects.get(pk=reqBody["user_id"]),
         route_id = Route.objects.get(pk=reqBody["route_id"])
     )
