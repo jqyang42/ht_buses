a49e00e65dc94cd55a1bffb6592593c39a6ed45f
Evelyn Cupil-Garcia <evelyn.cupil.garcia@gmail.com>
2022-04-06 12:50:09 -0400

    Added center and school coords

--- backend/ht_buses_app/views/buses/bus_active_view.py
+++ backend/ht_buses_app/views/buses/bus_active_view.py
@@ -1,51 +1,84 @@
-from ...models import Bus, Location, Log, User
-from ...serializers import BusSerializer, LocationSerializer, LogSerializer, UserSerializer
+from ...models import Bus, Location, Log, User, School, Route
+from ...serializers import BusSerializer, LocationSerializer, LogSerializer, RouteSerializer, SchoolSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from ...role_permissions import IsAdmin, IsDriver, IsSchoolStaff
 from datetime import timedelta
 
+# TODO: This method needs log permissions
 @csrf_exempt
 @api_view(["GET"])
 @permission_classes([IsAdmin|IsSchoolStaff|IsDriver]) 
 def get_buses(request):
     data = {}
     school_id = request.query_params["id"]
-    data["buses"] = active_buses_filter(school_id)
+    data = active_buses_filter(school_id)
     return Response(data)
 
 def active_buses_filter(school_id):
     data = {}
     bus_arr = []
+    school_arr = []
+    school_count = 0
+    avg_lat = 0
+    avg_lng = 0
     if int(school_id) == 0:
         buses = Bus.objects.filter(is_running=True)
-        bus_serializer = BusSerializer(buses, many=True)
-        for bus in bus_serializer.data:
-            bus_number = bus["bus_number"]
-            location_serializer = LocationSerializer(Location.objects.get(pk=bus["location_id"]), many=False)
-            bus_lat = location_serializer.data["lat"]
-            bus_lng = location_serializer.data["lng"]
-            location_arr = {"id": location_serializer.data["id"], 'lat': bus_lat, 'lng': bus_lng}
-            log_obj = Log.objects.filter(bus_number=bus_number, duration=timedelta(hours=0))
-            log_obj_serializer = LogSerializer(log_obj[0], many=False)
-            user_serializer = UserSerializer(User.objects.get(pk=log_obj_serializer.data["user_id"]), many=False)
-            user_arr = {'id': log_obj_serializer.data["user_id"], "first_name": user_serializer.data["first_name"], "last_name": user_serializer.data["last_name"]}
-            bus_arr.append({'bus_number': bus_number, 'location': location_arr, 'user': user_arr})
+        if len(buses) != 0:
+            bus_serializer = BusSerializer(buses, many=True)
+            for bus in bus_serializer.data:
+                bus_number = bus["bus_number"]
+                location_serializer = LocationSerializer(Location.objects.get(pk=bus["location_id"]), many=False)
+                bus_lat = location_serializer.data["lat"]
+                bus_lng = location_serializer.data["lng"]
+                location_arr = {"id": location_serializer.data["id"], 'lat': bus_lat, 'lng': bus_lng}
+                log_obj = Log.objects.filter(bus_number=bus_number, duration=timedelta(hours=0))
+                if len(log_obj) != 0:
+                    log_obj_serializer = LogSerializer(log_obj[0], many=False)
+                    route_obj = Route.objects.get(pk=log_obj_serializer.data["route_id"])
+                    route_serializer = RouteSerializer(route_obj, many=False)
+                    school_obj = School.objects.get(pk=route_serializer.data["school_id"])
+                    school_serializer = SchoolSerializer(school_obj, many=False)
+                    school_location = Location.objects.get(pk=school_serializer.data["location_id"])
+                    school_location_serializer = LocationSerializer(school_location, many=False)
+                    user_serializer = UserSerializer(User.objects.get(pk=log_obj_serializer.data["user_id"]), many=False)
+                    user_arr = {'id': log_obj_serializer.data["user_id"], "first_name": user_serializer.data["first_name"], "last_name": user_serializer.data["last_name"]}
+                    bus_arr.append({'bus_number': bus_number, 'location': location_arr, 'user': user_arr})
+                    school_location_arr = {'lat': school_location_serializer.data["lat"], 'lng': school_location_serializer.data["lng"]}
+                    school_arr.append({'id': school_serializer.data["id"], 'school': school_serializer.data["name"], 'location': school_location_arr})
+                    school_count += 1
+                    avg_lat += school_location_serializer.data["lat"]
+                    avg_lng += school_location_serializer.data["lng"]
     else:
         # grab buses from school
         logs = Log.objects.filter(route_id__school_id=school_id, duration=timedelta(hours=0))
-        log_serializer = LogSerializer(logs, many=True)
-        for log in log_serializer.data:
-            bus_number = log["bus_number"]
-            bus_obj = Bus.objects.filter(bus_number=bus_number)
-            bus_serializer_l = BusSerializer(bus_obj[0], many=False)
-            location_serializer_l = LocationSerializer(Location.objects.get(pk=bus_serializer_l.data["location_id"]), many=False)
-            bus_lat_l = location_serializer_l.data["lat"]
-            bus_lng_l = location_serializer_l.data["lng"]
-            location_arr = {"id": location_serializer_l.data["id"], 'lat': bus_lat_l, 'lng': bus_lng_l}
-            user_serializer = UserSerializer(User.objects.get(pk=log["user_id"]), many=False)
-            user_arr = {"id": log["user_id"], "first_name": user_serializer.data["first_name"], "last_name": user_serializer.data["last_name"]}
-            bus_arr.append({'bus_number': bus_number, 'location': location_arr, 'user': user_arr})
+        if len(logs) != 0:
+            log_serializer = LogSerializer(logs, many=True)
+            for log in log_serializer.data:
+                bus_number = log["bus_number"]
+                bus_obj = Bus.objects.filter(bus_number=bus_number)
+                bus_serializer_l = BusSerializer(bus_obj[0], many=False)
+                location_serializer_l = LocationSerializer(Location.objects.get(pk=bus_serializer_l.data["location_id"]), many=False)
+                bus_lat_l = location_serializer_l.data["lat"]
+                bus_lng_l = location_serializer_l.data["lng"]
+                location_arr = {"id": location_serializer_l.data["id"], 'lat': bus_lat_l, 'lng': bus_lng_l}
+                user_serializer = UserSerializer(User.objects.get(pk=log["user_id"]), many=False)
+                user_arr = {"id": log["user_id"], "first_name": user_serializer.data["first_name"], "last_name": user_serializer.data["last_name"]}
+                bus_arr.append({'bus_number': bus_number, 'location': location_arr, 'user': user_arr})
+            school_obj = School.objects.get(pk=school_id)
+            school_serializer = SchoolSerializer(school_obj, many=False)
+            school_location_obj = Location.objects.get(pk=school_serializer.data["location_id"])
+            school_location_serializer = LocationSerializer(school_location_obj, many=False)
+            school_location_arr = {'lat': school_location_serializer.data["lat"], 'lng': school_location_serializer.data["lng"]}
+            school_arr = {'id': school_id, 'name': school_serializer.data["name"], 'location': school_location_arr}
+            avg_lat += school_location_serializer.data["lat"]
+            avg_lng += school_location_serializer.data["lng"]
+            school_count += 1
+    if school_count != 0:
+        avg_lat = avg_lat / school_count
+        avg_lng = avg_lng / school_count
     data["buses"] = bus_arr
-    return bus_arr
+    data["schools"] = school_arr
+    data["center"] = {'lat': avg_lat, 'lng': avg_lng}
+    return data
